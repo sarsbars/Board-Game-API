3a1fd5d9a96f4950bda9a40732051712188a88df
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-09-22 14:02:44 -0500

    added Session Model

---
+++ Board Game API/Models/Session.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Board_Game_API.Models {
+    public class Session {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SessionID { get; set; }
+
+        [Required]
+        [ForeignKey("Game")]
+        public int GameID { get; set; }
+
+        public DateTime PlayDate { get; set; }
+        public int LengthOfTime { get; set; }
+        public string Summary { get; set; }
+
+        public virtual Game Game { get; set; }
+
+        public User Winner { get; set; }
+
+        public virtual ICollection<Collection> Collection { get; set; } = new List<Collection>();
+        public virtual ICollection<PlayParticipants> PlayParticpants { get; set; } = new List<PlayParticipants>();
+
+
+    }
+}
