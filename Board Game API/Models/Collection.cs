9ee5eeba67396ed3dd1426437d608e4bea8347bc
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-09-22 20:25:08 -0500

    created collection model

---
+++ Board Game API/Models/Collection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Board_Game_API.Models {
+    public class Collection {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CollectionID { get; set; }
+
+        [Required]
+        public int UserID { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual ICollection<CollectionGame> CollectionGames { get; set; } = new List<CollectionGame>();
+    }
+}
