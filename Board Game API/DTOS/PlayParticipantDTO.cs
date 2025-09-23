0867bf5952db40db6d2217f51c12041261c791cd
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-09-23 10:07:23 -0500

    created PlayParticipantDTO

---
+++ Board Game API/DTOS/PlayParticipantDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Board_Game_API.DTOS {
+    public class PlayParticipantDTO {
+        public int ParticipantID { get; set; }
+
+        [Required]
+        [ForeignKey("Session")]
+        public int SessionID { get; set; }
+
+        [Required]
+        [ForeignKey("User")]
+        public int UserID { get; set; }
+
+        public int? Score { get; set; }
+        public bool IsWinner { get; set; }
+
+    }
+}
