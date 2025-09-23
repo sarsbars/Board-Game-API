177b5980d14f53ed05a621a525c1e828695d3140
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-09-23 10:06:09 -0500

    created SessionDTO

---
+++ Board Game API/DTOS/SessionDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Board_Game_API.DTOS {
+    public class SessionDTO {
+        public int SessionID { get; set; }
+
+        [Required]
+        [ForeignKey("Game")]
+        public int GameID { get; set; }
+
+        [Required]
+        public DateTime PlayDate { get; set; }
+    }
+}
