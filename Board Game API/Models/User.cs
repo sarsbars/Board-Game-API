7ca9d83c882cc67da211fe1dbff2b3391bb1bee9
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-09-22 20:24:09 -0500

    created User Model

---
+++ Board Game API/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Board_Game_API.Models {
+    public class User {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public DateTime JoinDate { get; set; }
+
+        public int? CollectionID { get; set; }
+        public virtual Collection Collection { get; set; }
+        public virtual ICollection<PlayParticipant> PlayParticipants { get; set; } = new List<PlayParticipant>();
+    }
+}
