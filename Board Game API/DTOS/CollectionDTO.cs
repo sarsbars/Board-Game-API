aeb5ce7a29ca6c98e8723d5e9d4139a3c302a8e8
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-09-23 10:04:40 -0500

    created CollectionDTO

---
+++ Board Game API/DTOS/CollectionDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Board_Game_API.DTOS {
+    public class CollectionDTO {
+        public int CollectionID { get; set; }
+
+        [Required]
+        public int UserID { get; set; }
+    }
+}
