aa7b8d1f9d45fb0f770dd933c8b056f241ed6e4a
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-09-22 20:26:12 -0500

    created CollectionGame Model

---
+++ Board Game API/Models/CollectionGame.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Board_Game_API.Models {
+    public class CollectionGame {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CollectionGameID { get; set; }
+
+        [Required]
+        public int CollectionID { get; set; }
+
+        [Required]
+        public int GameID { get; set; }
+
+        [StringLength(30)]
+        public string Condition { get; set; }
+
+        [Range(1, 10)]
+        public int? PersonalRating { get; set; }
+
+        public virtual Collection Collection { get; set; }
+        public virtual Game Game { get; set; }
+    }
+}
