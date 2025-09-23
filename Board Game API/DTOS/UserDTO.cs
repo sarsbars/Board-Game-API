96459ec3b69c14566f0b19e7b868ea315cd0534e
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-09-23 10:01:56 -0500

    added UserDTO

---
+++ Board Game API/DTOS/UserDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Board_Game_API.DTOS {
+    public class UserDTO {
+        public int UserID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
