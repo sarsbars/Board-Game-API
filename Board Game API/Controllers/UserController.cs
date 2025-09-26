186a6463a249ec9a6d28c7eb1c1ecac45e807697
Connor Hall <connrhall204@gmail.com>
2025-09-26 09:34:20 -0500

    progress on delete method

--- Board Game API/Controllers/UserController.cs
+++ Board Game API/Controllers/UserController.cs
@@ -74,7 +74,15 @@ namespace Board_Game_API.Controllers {
         }
 
         //delete
-
-
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser (int id) {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null) {
+                return NotFound();
+            }
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
