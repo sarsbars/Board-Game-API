663f3c9c223971a3c73f80b36f16841bc3ebb119
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-09-25 19:36:51 -0500

    updated Mapping, as per Connor's fixes

--- Board Game API/Profiles/MappingProfile.cs
+++ Board Game API/Profiles/MappingProfile.cs
@@ -5,7 +5,14 @@ using Board_Game_API.Models;
 namespace Board_Game_API.Profiles {
     public class MappingProfile : Profile { 
         public MappingProfile() { 
-            CreateMap<Game, GameDTO>().ReverseMap(); 
+            CreateMap<Game, GameDTO>().ReverseMap();
+            CreateMap<Game, GameSummaryDTO>().ReverseMap();
+            CreateMap<Collection, CollectionDTO>().ReverseMap();
+            CreateMap<CollectionGame, CollectionGameDTO>().ReverseMap();
+            CreateMap<Session, SessionDTO>().ReverseMap();
+            CreateMap<User, UserDTO>().ReverseMap();
+            CreateMap<User, UserSummaryDTO>().ReverseMap();
+            CreateMap<PlayParticipant, PlayParticipantDTO>().ReverseMap();
         }
     }
 }
