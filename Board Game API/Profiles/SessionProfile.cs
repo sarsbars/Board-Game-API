abfe693aff4521c7cba95d8dfd6dbd6a76493ec3
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-09-25 21:16:33 -0500

    created SessionProfile

---
+++ Board Game API/Profiles/SessionProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Board_Game_API.DTOS;
+using Board_Game_API.Models;
+
+namespace Board_Game_API.Profiles {
+    public class SessionProfile : Profile {
+
+        public SessionProfile() {
+
+            CreateMap<Session, SessionDTO>().ReverseMap();
+        }
+    }
+}
