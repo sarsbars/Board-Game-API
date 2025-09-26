7b99dc165d637456f3472a9b0dfe2cc38e63f96c
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-09-25 21:17:41 -0500

    created PlayParticipantProfile

---
+++ Board Game API/Profiles/PlayParticipantProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Board_Game_API.DTOS;
+using Board_Game_API.Models;
+
+namespace Board_Game_API.Profiles {
+    public class PlayParticipantProfile : Profile {
+
+        public PlayParticipantProfile() {
+
+            CreateMap<PlayParticipant, PlayParticipantDTO>().ReverseMap();
+        }
+    }
+}
