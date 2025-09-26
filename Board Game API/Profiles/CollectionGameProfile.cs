cd298eca6d52d909f705e4ad6216ca32e8b085b1
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-09-25 21:10:50 -0500

    created CollectionGameProfile

---
+++ Board Game API/Profiles/CollectionGameProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Board_Game_API.DTOS;
+using Board_Game_API.Models;
+
+namespace Board_Game_API.Profiles {
+    public class CollectionGameProfile : Profile {
+        
+        public CollectionGameProfile() {
+
+            CreateMap<CollectionGame, CollectionGameDTO>().ReverseMap();
+
+        }
+    }
+}
